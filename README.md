b5078a79d4c509ea689d5b89460fbfe77bc72749
CharleyMcLean <charleyaustinmclean@gmail.com>
2017-01-26 15:12:31 -0800

    Added input and output files, as well as requirements.  Added README (missing explanation of second algorithm).  First algorithm working.

---
+++ README.md
@@ -0,0 +1,108 @@
+# Cafe Shoppe
+
+There are exactly 2 baristas, with ID numbers ```[1, 2]```. A barista can make at most 1 drink at a time. For example, if a barista starts making a ```3-minute tea``` at ```time=5```, she will deliver the drink at ```time=8``` and can start making a new drink at ```time=8```.
+
+The work day starts at ```time=0``` and ends at ```time=100```.
+
+There are three types of drinks, each of which takes a different amount of time to make and yields a different profit.
+
+```
+[
+    { "type": "tea",      "brew_time": 3, "profit": 2 },
+    { "type": "latte",    "brew_time": 4, "profit": 3 },
+    { "type": "affogato", "brew_time": 7, "profit": 5 }
+]
+```
+
+##Contents
+* [Technologies](#technologies)
+* [Example Input/Output of the Program](#example_in_and_out)
+* [Deliverables](#deliverables)
+* [Explanation of Second Algorithm](#explanation)
+* [Installation](#install)
+
+## <a name="technologies"></a>Technologies
+
+Tech Stack: Python with json library<br/>
+
+
+## <a name="example_in_and_out"></a>Example Input/Output of the Program
+
+Below is example input data of orders made in a given day, ordered by order_time. See file input.json for a full list.
+
+```
+[
+    { "order_id": 1, "order_time": 0, "type": "affogato" },
+    { "order_id": 2, "order_time": 1, "type": "tea" },
+    { "order_id": 3, "order_time": 2, "type": "latte" },
+    { "order_id": 4, "order_time": 2, "type": "tea" }
+]
+```
+
+Below is example output data. See file output_fifo.json for the expected output given input.json. The output of the solutions should include drinks that each barista makes and the time that the barista starts making the drink. It must be valid json - an array of objects with the following format:
+
+```
+[
+    { "order_id": 1, "start_time": 0, "barista_id": 1 },
+    { "order_id": 2, "start_time": 1, "barista_id": 2 },
+    { "order_id": 3, "start_time": 4, "barista_id": 2 },
+    { "order_id": 4, "start_time": 7, "barista_id": 1 }
+]
+```
+
+
+## <a name="deliverables"></a>Deliverables
+
+Design 2 solutions that handle the drink orders. The first is a simple first-in-first-out (FIFO) solution, the second is an algorithm of your choice.
+
+1. Implement a FIFO (first-in-first-out) solution where a drink is immediately assigned to the first available barista. The output must match the json output in ```output_fifo.json``` (spacing doesn't matter). The drink orders are in ```input.json```.
+
+2. Implement a second algorithm that would make the Cafe Shoppe more successful. Use creativity to optimize for a business need. Examples of things to think about: # of drinks made (throughput), shortest average customer wait time, most profit made, etc. Document your reasoning in your README.
+
+**Caveat**:
+
+* not all drinks will necessarily be completed
+* for the FIFO solution, all orders processed before or at time=100 will be delivered to the customer, even if the drinks are delivered after time=100.
+
+
+## <a name="explanation"></a>Explanation of Second Algorithm
+
+Explanation of second algorithm will go here...
+
+
+## <a name="install"></a>Installation
+
+To run Cafe Shoppe:
+
+Clone or fork this repo:
+
+```
+https://github.com/CharleyMcLean/cafe-shoppe.git
+```
+
+Create and activate a virtual environment inside your Cafe Shoppe directory:
+
+```
+virtualenv env
+source env/bin/activate
+```
+
+Install the dependencies:
+
+```
+pip install -r requirements.txt
+```
+
+Run the file with the algorithm you'd like to test:
+
+```
+python coffee_shop_1.py
+```
+
+**or**
+
+```
+python coffee_shop_.py
+```
+
+The orders output will print to the terminal.
--- coffee_shop_1.py
+++ coffee_shop_1.py
@@ -8,8 +8,7 @@ coffee_queue = Queue()
 emp1 = Employee(1)
 emp2 = Employee(2)
 
-TOTAL_TIME = 100
-
+# Global variable to hold drink types and their characteristics
 DRINKS = [
     {"type": "tea", "brew_time": 3, "profit": 2},
     {"type": "latte", "brew_time": 4, "profit": 3},
@@ -17,8 +16,21 @@ DRINKS = [
 ]
 
 
+# Load and parse the json file.
+# The file name in "" may be changed to test other data sets
+order_data = []
+with open("input.json") as json_file:
+    # Use a try/except in case the json file is empty
+    try:
+        json_data = json.load(json_file)
+        for order in json_data:
+            order_data.append(order)
+    except:
+        order_data = []
+
+
 def fill_queue(orders_of_the_day):
-    """Define a function to fill the queue for the day."""
+    """Define a function to fill the queue with the orders of the day."""
     for order in orders_of_the_day:
         coffee_queue.enqueue(order)
     return coffee_queue
@@ -30,9 +42,13 @@ def make_drinks(coffee_queue):
     start_time, and barista_id."""
     # drinks_made = []
     # ex: next = {"order_id": 1, "order_time": 0, "type": "affogato"}
-    next = coffee_queue.dequeue()
+    next = {}
     time_counter = 0
     drinks_made = []
+    if coffee_queue.isEmpty():
+        return drinks_made
+    if not coffee_queue.isEmpty():
+        next = coffee_queue.dequeue()
 
     while time_counter < 101:
         if emp1.time_avail <= time_counter:
@@ -63,9 +79,10 @@ def make_drinks(coffee_queue):
             else:
                 start_time = emp2.time_avail
 
-            drinks_made.append({"order_id": next["order_id"],
+            drinks_made.append({"barista_id": emp2.emp_id,
                                 "start_time": start_time,
-                                "barista_id": emp2.emp_id})
+                                "order_id": next["order_id"]
+                                })
 
             brew_time = find_brew_time(next["type"])
             emp2.time_avail += brew_time
@@ -81,7 +98,7 @@ def make_drinks(coffee_queue):
 
 
 def find_brew_time(order_type):
-    """helper function to determine brew time"""
+    """Helper function to determine brew time"""
     brew_time = int()
     for drink in DRINKS:
         if order_type == drink["type"]:
@@ -90,202 +107,7 @@ def find_brew_time(order_type):
     return brew_time
 
 
-# def check_employee_status(emp):
-#     if emp.time_avail <= time_counter:
-#     # if the order is placed after the employee becomes available
-#     # the start time will be when the order is placed
-#         if next["order_time"] > emp.time_avail:
-#             start_time = next["order_time"]
-#         else:
-#             start_time = emp.time_avail
-
-#         drinks_made.append({"order_id": next["order_id"],
-#                             "start_time": start_time,
-#                             "barista_id": emp.emp_id})
-
-#         brew_time = find_brew_time(next["type"])
-#         emp1.time_avail += brew_time
-
-#         if coffee_queue.isEmpty():
-#             break
-#         else:
-#             next = coffee_queue.dequeue()
-
-
-#     for i in range(TOTAL_TIME + 1):
-#         # i is the current time
-#         # emp1.time tells us at what time the employee becomes available
-#         # if the drink is ordered at or after the time the emp becomes available
-#         # if the actual time is at or after the time the emp becomes available
-#         print i
-
-#         if next["order_time"] >= emp1.time:
-#             complete_next_order(emp1, next)
-
-#             # start_time1 = int()
-#             # if next["order_time"] >= emp1.time:
-#             #     start_time = next["order_time"]
-#             # else:
-#             #     start_time = emp1.time
-#             # print "start_time is:", start_time
-#             # drinks_made.append({"order_id": next["order_id"],
-#             #                     "start_time": start_time1,
-#             #                     "barista_id": emp1.emp_id})
-#             # print "after emp1, drinks_made=", drinks_made
-
-#             # next_drink_type = {}
-#             # for drink in DRINKS:
-#             #     if next["type"] == drink["type"]:
-#             #         next_drink_type = drink
-#             # print "about to print emp1.time"
-#             # emp1.time = start_time + next_drink_type["brew_time"]
-#             # print emp1.time
-
-#             # if not coffee_queue.isEmpty():
-#             #     next = coffee_queue.dequeue()
-#             # else:
-#             #     continue
-
-#         if next["order_time"] >= emp2.time:
-#             complete_next_order(emp2, next)
-
-#             # start_time2 = int()
-#             # if next["order_time"] >= emp2.time:
-#             #     start_time = next["order_time"]
-#             # else:
-#             #     start_time = emp2.time
-
-#             # drinks_made.append({"order_id": next["order_id"],
-#             #                     "start_time": start_time2,
-#             #                     "barista_id": emp2.emp_id})
-#             # print "after emp2, drinks_made=", drinks_made
-#             # next_drink_type = {}
-#             # for drink in DRINKS:
-#             #     if next["type"] == drink["type"]:
-#             #         next_drink_type = drink
-#             # print "about to print emp2.time"
-#             # emp2.time = start_time + next_drink_type["brew_time"]
-#             # print emp2.time
-
-#             # if not coffee_queue.isEmpty():
-#             #     next = coffee_queue.dequeue()
-#             # else:
-#             #     continue
-
-#         # i = min(start_time1, start_time2)
-#         print i
-#     return drinks_made
-
-# def complete_next_order(emp, next):
-#     """Helper function"""
-#     drinks_made = []
-#     start_time = int()
-
-#     if next["order_time"] >= emp.time:
-#         start_time = next["order_time"]
-#     else:
-#         start_time = emp.time
-#     print "start_time is:", start_time
-
-#     drinks_made.append({"order_id": next["order_id"],
-#                         "start_time": start_time,
-#                         "barista_id": emp.emp_id})
-#     print "after emp, drinks_made=", drinks_made
-
-#     next_drink_type = {}
-#     for drink in DRINKS:
-#         if next["type"] == drink["type"]:
-#             next_drink_type = drink
-#     print "about to print emp.time"
-#     emp.time = start_time + next_drink_type["brew_time"]
-#     print emp.time
-
-#     if not coffee_queue.isEmpty():
-#         next = coffee_queue.dequeue()
-
-#     return drinks_made
-
-
-#     #     if emp1.available and next["order_time"] >= i and emp1.time <= i:
-#     #         emp1.available = False
-
-#     #         start_time = 0
-#     #         if i > emp1.time:
-#     #             start_time = i
-#     #         else:
-#     #             start_time = emp1.time
-
-#     #         drinks_made.append({"order_id": next["order_id"],
-#     #                             "start_time": start_time,
-#     #                             "barista_id": emp1.emp_id})
-#     #         next_drink = {}
-#     #         for drink in DRINKS:
-#     #             if next["type"] == drink["type"]:
-#     #                 next_drink = drink
-
-#     #         emp1.time = start_time + next_drink["brew_time"]
-#     #         emp1.available = True
-#     #         # if not coffee_queue.isEmpty():
-#     #         #     next = coffee_queue.dequeue()
-
-#     #     if emp2.available and next["order_time"] >= i and emp2.time <= i:
-#     #         emp2.available = False
-#     #         start_time = 0
-#     #         if i > emp2.time:
-#     #             start_time = i
-#     #         else:
-#     #             start_time = emp2.time
-
-#     #         drinks_made.append({"order_id": next["order_id"],
-#     #                             "start_time": start_time,
-#     #                             "barista_id": emp2.emp_id})
-#     #         next_drink = {}
-#     #         for drink in DRINKS:
-#     #             if next["type"] == drink["type"]:
-#     #                 next_drink = drink
-
-#     #         emp2.time = start_time + next_drink["brew_time"]
-#     #         emp2.available = True
-#     #         if not coffee_queue.isEmpty():
-#     #             next = coffee_queue.dequeue()
-
-#     #     # if not emp1.available and emp1.time <= i:
-#     #     #     emp1.available = True
-#     #     # if not emp2.available and emp2.time <= i:
-#     #     #     emp2.available = True
-
-#     # return drinks_made
-
-
-
-
-
-
-
-
-
-
-
-#     # drinks_made = []
-#     # # ex: next = {"order_id": 1, "order_time": 0, "type": "affogato"}
-#     # next = coffee_queue.dequeue()
-#     # for i in range(coffee_queue.size()):
-#     #     if emp_1.available and next["order_time"] >= emp_1.time:
-#     #         # Append order info to list of daily orders
-#     #         emp_1.available = False
-#     #         drinks_made.append({"order_id": next["order_id"],
-#     #                             "start_time": emp_1.time,
-#     #                             "barista_id": emp_1.emp_id})
-#     #         emp_1.time += next["order_time"]
-#     #         emp_1.available = True
-
-#     #     elif emp_2.available and next["order_time"] >= emp_2.time:
-#     #         emp_2.available = False
-#     #         drinks_made.append({"order_id": next["order_id"],
-#     #                             "start_time": emp_2.time,
-#     #                             "barista_id": emp_2.emp_id})
-#     #         emp_2.time += next["order_time"]
-#     #         emp_2.available = True
-#     #     next = coffee_queue.dequeue()
-
-#     # return drinks_made
+#####################################################################
+# Fill queue with orders of the day, and generate output data.
+fill_queue(order_data)
+print make_drinks(coffee_queue)
